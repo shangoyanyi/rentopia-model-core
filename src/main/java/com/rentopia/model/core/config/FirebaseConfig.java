35b79ebdc3e139e803854a60693690369269b28d
shangoyanyi <shangoyanyi@gmail.com>
2022-03-23 21:35:31 +0800

    read firebase credential from env

--- src/main/java/com/rentopia/model/core/config/FirebaseConfig.java
+++ src/main/java/com/rentopia/model/core/config/FirebaseConfig.java
@@ -1,6 +1,9 @@
 package com.rentopia.model.core.config;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +22,8 @@ public class FirebaseConfig {
 	@Bean
 	public void initFirebaseAppBean() throws Exception {
 		try {
-        	// when reading a file under resources folder, we need to push a slash in front of the file path
-	        InputStream firebaseCredential = this.getClass().getResourceAsStream("/credentials/firebase-credential.json");
-	
-	        log.info("firebaseCredential:" + firebaseCredential);
-	        
+			InputStream firebaseCredential = this.getFirebaseCredential();
+ 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 			  .setCredentials(GoogleCredentials.fromStream(firebaseCredential))
 			  .build();
@@ -38,6 +38,55 @@ public class FirebaseConfig {
         	log.error(e.toString());
         	throw e;
         }
+	}
+	
+	
+	/**
+	 * get firebase-credentil from project folder or from env variable value
+	 * @return InputStream
+	 * @throws Exception
+	 */
+	private InputStream getFirebaseCredential() throws Exception {
+		InputStream firebaseCredential = null;
+		try {
+			/*
+			 * 1st: read credential from folder (for develop)
+			 */
+			// when reading a file under resources folder, we need to put a slash in front of the file path
+	        firebaseCredential = this.getClass().getResourceAsStream("/credentials/firebase-credential.json");
+	        log.info("firebaseCredential:" + firebaseCredential);
+	        
+	        if(firebaseCredential!=null) {
+	        	return firebaseCredential;
+	        }
+	        
+			
+	        /*
+	         * 2nd: read credential from environment (for build and runtime)
+	         */
+	        String firebaseCredentialStringInBase64 = System.getenv().getOrDefault("firebase-credential", "");
+			log.info("firebaseCredentialStringInBase64 = " + firebaseCredentialStringInBase64);
+			
+			if(!"".equals(firebaseCredentialStringInBase64)) {
+				String firebaseCredentialString = new String(Base64.getDecoder().decode(firebaseCredentialStringInBase64), "utf-8");
+				log.info("firebaseCredentialString = " + firebaseCredentialString);
+			
+				firebaseCredential = new ByteArrayInputStream(firebaseCredentialString.getBytes());
+				log.info("firebaseCredential:" + firebaseCredential);
+				
+				return firebaseCredential;
+			}
+			
+			
+			/*
+			 * 3rd: no any credential to use
+			 */
+			throw new Exception("error: no firebase-credential.json file or env variable");
+			
+		}catch(Exception e) {
+			log.error(e.toString());
+			throw e;
+		}
 	}
 
 }
